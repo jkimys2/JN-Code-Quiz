a3c0837aeb0eec754187aae47d6dd84b84950928
Jordan <jkimys2@gmail.com>
2023-12-07 12:14:56 -0500

    update

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var startEl = document.querySelector('.start-quiz');
+var questionsEl = document.querySelector('.questions');
+var allDoneEl = document.querySelector('.all-done');
+var highscores = document.querySelector('.highscores');
+
