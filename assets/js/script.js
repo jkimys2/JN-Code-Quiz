5a1ede5fe3cf449ad4c2098920ac647ce71c1935
Jordan <jkimys2@gmail.com>
2023-12-07 12:42:06 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,15 @@ var timer;
 // these are the array of questions
 var questions = [
     'What does HTML stand for?',
-    ''
+    'Which of these are the correct HTML element for the largest heading?',
+    'Which character is used to indicate an end tag?',
+    'What does CSS stand for?',
+    'Which CSS property controls the text size?',
+    'How do you select elements with a class name of "test"?',
+    'Which HTML element does JavaScript go in?',
+    'True or False: JavaScript is the same as Java.',
+    'Which operator is used to assign a value to a variable?',
+    'True or False: JavaScript is NOT case-sensitive.'
 ]
 
 function startQuiz() {
