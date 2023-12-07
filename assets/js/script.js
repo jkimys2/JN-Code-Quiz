34fb8b71f944b630d93a34772e6d54d9ca18caca
Jordan <jkimys2@gmail.com>
2023-12-07 12:26:02 -0500

    css and js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+// these are the variables that call the html elements
 var startEl = document.querySelector('.start-quiz');
 var questionsEl = document.querySelector('.questions');
 var allDoneEl = document.querySelector('.all-done');
 var highscores = document.querySelector('.highscores');
 
+var questions = '';
+var answerChoices = '';
+var timer;
+
+// these are the array of questions
+var questions = [
+    'What does HTML stand for?',
+    ''
+]
+
+function startQuiz() {
+
+}
